449d9e0ccc5280d354a42466f5407e9451df7380
kautukdwivedi <kautukdwivedi@icloud.com>
2021-05-15 16:11:33 +0530

    impliment -n

--- sender.js
+++ sender.js
@@ -1,24 +1,22 @@
 #!/usr/bin/env node
 const puppeteer = require("puppeteer");
 
-let cmd=process.argv[2];
-if(cmd == "-help")  
-  showhelp();
+let cmd = process.argv[2];
+if (cmd == "-help") showhelp();
 let args = process.argv.slice(3);
 let idx = 0;
 let message = "";
 let contacts = [];
-for(;args[idx]!="-c";idx++){
+for (; args[idx] != "-c"; idx++) {
   message = message + " " + args[idx];
 }
 idx++;
-for(;idx<args.length;idx++){
-  contacts.push(args[idx])
+for (; idx < args.length; idx++) {
+  contacts.push(args[idx]);
 }
 
 (async function () {
-  if(cmd == "-help")  
-  await showhelp();
+  if (cmd == "-help") await showhelp();
   let browser = await puppeteer.launch({
     executablePath: "/usr/bin/brave-browser",
     headless: false,
@@ -30,7 +28,8 @@ for(;idx<args.length;idx++){
   if (cmd === "-m") await sendMessage(tab, message);
   else if (cmd === "-g") await sendGif(tab, message);
   else if (cmd === "-r") await sendReaction(tab, message);
-  else if (cmd === "-w") await sendGreetings(tab, message,browser);
+  else if (cmd === "-w") await sendGreetings(tab, message, browser);
+  else if (cmd === "-n") await createGroup(tab, message);
   else await showhelp();
   process.exit(1);
 })();
@@ -47,21 +46,21 @@ async function sendMessage(tab, message) {
   }
 }
 
-async function sendGreetings(tab, greeting,browser) {
+async function sendGreetings(tab, greeting, browser) {
   await tab.goto("https://createcustomwishes.com/");
   await tab.waitForSelector(".fa.fa-search");
   await tab.click(".fa.fa-search");
   await tab.waitForSelector(".search-field");
-  await tab.type(".search-field",greeting);
+  await tab.type(".search-field", greeting);
   await tab.waitForSelector('[value="Search"]');
   await tab.click('[value="Search"]');
   await tab.waitForNavigation({ waitUntil: "networkidle2" });
   await tab.waitForSelector(".entry-summary p");
   let greetContent = await tab.evaluate(function () {
-    return document.querySelector('.entry-summary p').textContent;
+    return document.querySelector(".entry-summary p").textContent;
   });
   let newTab = await browser.newPage();
-  await sendMessage(newTab,greetContent);
+  await sendMessage(newTab, greetContent);
 }
 
 async function sendGif(tab, topic) {
@@ -77,10 +76,12 @@ async function sendGif(tab, topic) {
     await tab.waitForSelector("._5o68e.copyable-text.selectable-text");
     await tab.type("._5o68e.copyable-text.selectable-text", topic);
     await tab.keyboard.press("Enter");
+    await tab.waitForTimeout(1000);
     await tab.waitForSelector("._1qWwG");
     await tab.click("._1qWwG", "right");
     await tab.waitForSelector(".SncVf._3doiV");
     await tab.click(".SncVf._3doiV", "right");
+    await tab.waitForTimeout(1000);
   }
 }
 
@@ -112,14 +113,42 @@ async function sendReaction(tab, reaction) {
   }
 }
 
+async function createGroup(tab, name) {
+  await tab.goto("https://web.whatsapp.com/");
+  await tab.waitForSelector('[aria-label="Menu"]');
+  await tab.click('[aria-label="Menu"]', "left");
+  await tab.waitForSelector('[aria-label="New group"]');
+  await tab.click('[aria-label="New group"]');
+  await tab.waitForSelector("._38sK8.copyable-text.selectable-text");
+  for (let i = 0; i < contacts.length; i++) {
+    await tab.type("._38sK8.copyable-text.selectable-text", contacts[i]);
+    await tab.keyboard.press("Enter");
+    await tab.waitForTimeout(1000);
+  }
+  await tab.keyboard.press("Enter");
+  await tab.waitForTimeout(1000);
+  await tab.waitForSelector('[data-tab="6"]');
+  await tab.type('[data-tab ="6"]', name);
+  await tab.keyboard.press("Enter");
+  await tab.waitForSelector("._3xWLK");
+  await tab.click("._3xWLK");
+  await tab.waitForSelector(".SncVf._3doiV");
+  await tab.click(".SncVf._3doiV");
+}
+
 async function showhelp() {
-  console.log("Format                :- w-app -cmd [Message] -c [Contact list must be seperated by space]");
+  console.log(
+    "Format                :- w-app -cmd [Message] -c [Contact list must be seperated by space]"
+  );
   console.log("-m [Message]          :- To send message to given contacts");
   console.log("-g [Topic]            :- To send gif to given contacts");
   console.log(
     "-r [Emoji]            :- To give reactions using emoji to given contacts"
   );
-  console.log("-w [Ocassion]         :- To send greeting wishes to given contacts");
+  console.log(
+    "-w [Ocassion]         :- To send greeting wishes to given contacts"
+  );
+  console.log("-n [Name]             :- To make new group for given contacts");
   console.log("-help                 :- show help");
   process.exit(1);
 }
